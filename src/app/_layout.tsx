726a4144c52359b0763e61994f705dc90518c91d
Prof. Renato Oliveira <162147229+profRenatoOliveira1@users.noreply.github.com>
2026-05-27 14:04:25 -0300

    praticando navegação em abas

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,47 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Tabs>
+      <Tabs.Screen name="index" 
+        options={{ 
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons 
+              name={focused ? "home" : "home-outline"}
+              size={24} 
+              color="blue" 
+            />
+          ),
+          headerShown: false
+        }} />
+      <Tabs.Screen name="settings"
+        options={{
+          title: "Configurações",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons 
+              name={focused ? "settings" : "settings-outline"}
+              size={24} 
+              color="blue" 
+            />
+          ),
+          headerShown: false
+        }}
+      />
+      <Tabs.Screen name="ajuda"
+        options={{
+          title: "Ajuda",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons 
+              name={focused ? "help-circle" : "help-circle-outline"}
+              size={24} 
+              color="blue" 
+            />
+          ),
+          headerShown: false
+        }}
+      />
+    </Tabs>
+  );
 }

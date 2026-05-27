fea578f4a42dfd03987fa94895d00861854cfe11
Prof. Renato Oliveira <162147229+profRenatoOliveira1@users.noreply.github.com>
2026-05-27 14:22:21 -0300

    aplicando navegação drawer

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,47 +1,68 @@
 import { Tabs } from "expo-router";
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { Drawer } from 'expo-router/drawer';
 
 export default function RootLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" 
-        options={{ 
-          title: "Home",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons 
-              name={focused ? "home" : "home-outline"}
-              size={24} 
-              color="blue" 
-            />
-          ),
-          headerShown: false
-        }} />
-      <Tabs.Screen name="settings"
+    <Drawer>
+      <Drawer.Screen name="index"
         options={{
-          title: "Configurações",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons 
-              name={focused ? "settings" : "settings-outline"}
-              size={24} 
-              color="blue" 
-            />
-          ),
-          headerShown: false
+          title: "Ínicio"
         }}
       />
-      <Tabs.Screen name="ajuda"
+      <Drawer.Screen name="settings"
         options={{
-          title: "Ajuda",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons 
-              name={focused ? "help-circle" : "help-circle-outline"}
-              size={24} 
-              color="blue" 
-            />
-          ),
-          headerShown: false
+          title: "Configurações"
         }}
       />
-    </Tabs>
+      <Drawer.Screen name="ajuda" 
+        options={{
+          title: "Ajuda"
+        }}
+      />
+    </Drawer>
   );
+  
+  // (
+  //   <Tabs>
+  //     <Tabs.Screen name="index" 
+  //       options={{ 
+  //         title: "Home",
+  //         tabBarIcon: ({ color, focused }) => (
+  //           <Ionicons 
+  //             name={focused ? "home" : "home-outline"}
+  //             size={24} 
+  //             color="blue" 
+  //           />
+  //         ),
+  //         headerShown: false
+  //       }} />
+  //     <Tabs.Screen name="settings"
+  //       options={{
+  //         title: "Configurações",
+  //         tabBarIcon: ({ color, focused }) => (
+  //           <Ionicons 
+  //             name={focused ? "settings" : "settings-outline"}
+  //             size={24} 
+  //             color="blue" 
+  //           />
+  //         ),
+  //         headerShown: false
+  //       }}
+  //     />
+  //     <Tabs.Screen name="ajuda"
+  //       options={{
+  //         title: "Ajuda",
+  //         tabBarIcon: ({ color, focused }) => (
+  //           <Ionicons 
+  //             name={focused ? "help-circle" : "help-circle-outline"}
+  //             size={24} 
+  //             color="blue" 
+  //           />
+  //         ),
+  //         headerShown: false
+  //       }}
+  //     />
+  //   </Tabs>
+  // );
 }
